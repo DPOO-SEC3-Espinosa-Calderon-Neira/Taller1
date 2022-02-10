34a1c20abe4be9f4ca19198802b3e14531dd8b95
jneiracampos <j.neira@uniandes.edu.co>
2022-02-09 20:43:06 -0500

    Cambios

--- Taller 1 - Hamburguesas/src/consola/Aplicacion.java
+++ Taller 1 - Hamburguesas/src/consola/Aplicacion.java
@@ -3,7 +3,7 @@ package consola;
 public class Aplicacion {
 	
 	public static void main (String[] args) {
-		
+		System.out.println("Seleccione una de las siguientes opciones:");
 	}
 	
 	public void mostrarMenu() {
