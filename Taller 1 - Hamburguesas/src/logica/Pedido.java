b0e9cebec90cbb130328217cf2ba8b3225d2811b
jneiracampos <j.neira@uniandes.edu.co>
2022-02-10 20:04:02 -0500

    ...

--- Taller 1 - Hamburguesas/src/logica/Pedido.java
+++ Taller 1 - Hamburguesas/src/logica/Pedido.java
@@ -1,43 +1,42 @@
 package logica;
 
 public class Pedido {
-	
-	private int numeroPedidos;
+
+	private static int numeroPedidos;
 	private int idPedido;
 	private String nombreCliente;
 	private String direccionCliente;
-	
+
 	public Pedido(String nombreCliente, String direccionCliente) {
 		this.nombreCliente = nombreCliente;
 		this.direccionCliente = direccionCliente;
-		
 	}
-	
+
 	public int getIdPedido() {
 		return idPedido;
 	}
-	
+
 	public void agregarProducto(Producto nuevoItem) {
-		
+
 	}
-	
+
 	private int getPrecioNetoPedido() {
-		
+
 	}
-	
+
 	private int getPrecioTotalPedido() {
-		
+
 	}
-	
+
 	private int getPrecioIVAPedido() {
-		
+
 	}
-	
+
 	private String generarTextoFactura() {
-		
+
 	}
-	
+
 	public void guardarFactura(String archivo) {
-		
+
 	}
 }
--- Taller 1 - Hamburguesas/src/logica/Restaurante.java
+++ Taller 1 - Hamburguesas/src/logica/Restaurante.java
@@ -1,6 +1,5 @@
 package logica;
 
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -11,49 +10,51 @@ import java.util.List;
 import java.util.Map;
 
 public class Restaurante {
-	
+
 	private Pedido pedido;
 	private Ingrediente ingrediente;
+
+	
 	
 	public Restaurante() {
-		
+
 	}
-	
+
 	public void iniciarPedido(String nombreCliente, String direccionCliente) {
 		Pedido nuevoPedido = new Pedido(nombreCliente, direccionCliente);
 	}
-	
+
 	public void cerrarYGuardarPedido() {
-		
+
 	}
-	
+
 	public Pedido getPedidoEnCurso() {
 		return pedido;
 	}
-	
+
 	public ArrayList<Producto> getMenuBase() {
-		
+
 	}
-	
+
 	public ArrayList<Ingrediente> getIngredientes() {
-		
+
 	}
-	
-	public void cargarInfoRestaurante(String archivoIngredientes, String archivoMenu,  String archivoCombos) {
+
+	public void cargarInfoRestaurante(String archivoIngredientes, String archivoMenu, String archivoCombos) {
 		cargarIngredientes(archivoIngredientes);
 		cargarMenu(archivoMenu);
 		cargarCombos(archivoCombos);
 	}
-	
+
 	private void cargarIngredientes(String archivoIngredientes) {
-		
+
 	}
-	
+
 	private void cargarMenu(String archivoMenu) {
-		
+
 	}
 
 	private void cargarCombos(String archivoCombos) {
-	
+
 	}
 }
