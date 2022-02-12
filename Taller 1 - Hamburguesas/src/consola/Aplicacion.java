44efe5b1e13aba4caa6d669bfa450cadbb065c52
jneiracampos <j.neira@uniandes.edu.co>
2022-02-12 15:48:16 -0500

    Cambios

--- Taller 1 - Hamburguesas/src/consola/Aplicacion.java
+++ Taller 1 - Hamburguesas/src/consola/Aplicacion.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 import logica.Restaurante;
 
 public class Aplicacion {
-	
+
 	private Restaurante restaurante = new Restaurante();
-	
+
 	public static void main(String[] args) throws IOException {
 		Aplicacion consola = new Aplicacion();
 		consola.cargarArchivos();
 		consola.ejecutarOpcion();
-		
+
 	}
-	
+
 	public void cargarArchivos() throws IOException {
-		restaurante.cargarInfoRestaurante
-				("C:\\Users\\danie\\OneDrive\\Documentos\\GitHub\\Taller1\\Taller 1 - Hamburguesas\\data\\ingredientes.txt", 
+		restaurante.cargarInfoRestaurante(
+				"C:\\Users\\danie\\OneDrive\\Documentos\\GitHub\\Taller1\\Taller 1 - Hamburguesas\\data\\ingredientes.txt",
 				"C:\\Users\\danie\\OneDrive\\Documentos\\GitHub\\Taller1\\Taller 1 - Hamburguesas\\data\\menu.txt",
 				"C:\\Users\\danie\\OneDrive\\Documentos\\GitHub\\Taller1\\Taller 1 - Hamburguesas\\data\\combos.txt");
 	}
 
 	public void ejecutarOpcion() {
-		
+
 		Scanner input = new Scanner(System.in);
 		boolean continuar = true;
 		while (continuar) {
@@ -40,11 +40,10 @@ public class Aplicacion {
 				else if (opcion_seleccionada == 4)
 					consultar_pedido();
 				else if (opcion_seleccionada == 5) {
-					
+
 					System.out.println("Saliendo de la aplicación...");
 					continuar = false;
-				} 
-				else
+				} else
 					System.out.println("Por favor seleccione una de las opciones");
 			} catch (NumberFormatException e) {
 				System.out.println("Debe seleccionar uno de los números de las opciones");
@@ -52,7 +51,7 @@ public class Aplicacion {
 			input.close();
 		}
 	}
-	
+
 	public void mostrarMenu() {
 		System.out.println("\nOpciones ");
 		System.out.println("1. Hacer un pedido");
@@ -61,17 +60,24 @@ public class Aplicacion {
 		System.out.println("4. Condsultar pedido");
 		System.out.println("5. Salir");
 		System.out.println("\nOpciones ");
-		
+
 	}
-	
+
 	private void iniciar_pedido() {
 		
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.println("\nIngrese su nombre: ");
+			String nombre = input.nextLine();
+			System.out.println("\nIngrese su dirección: ");
+			String direccion = input.nextLine();
+			restaurante.iniciarPedido(nombre, direccion);
+		}
 	}
-	
+
 	private void agregar_elemento() {
 
 	}
-	
+
 	private void finalizar_pedido() {
 
 	}
