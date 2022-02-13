19e6c0a23f88060f70a931ae7724625ff8a7f290
despinosaa <d.espinosaa@uniandes.edu.co>
2022-02-13 00:00:11 -0500

    más mejoras

--- Taller 1 - Hamburguesas/src/consola/Aplicacion.java
+++ Taller 1 - Hamburguesas/src/consola/Aplicacion.java
@@ -16,6 +16,7 @@ public class Aplicacion {
 	private Restaurante restaurante = new Restaurante();
 	private Pedido pedido;
 	private double total = 0;
+	private String modificacion = "";
 	
 	public static void main(String[] args) throws IOException {
 		Aplicacion consola = new Aplicacion();
@@ -71,8 +72,8 @@ public class Aplicacion {
 	}
 	
 	private void iniciar_pedido() {
-		String nombreCliente = input("\nPor favor ingresa su nombre");
-		String direccionCliente = input("Por favor ingresa su direccion");
+		String nombreCliente = input("\nPor favor ingresa tu nombre");
+		String direccionCliente = input("Por favor ingresa tu direccion");
 		pedido = new Pedido(nombreCliente, direccionCliente);
 		System.out.println("\nHola " + nombreCliente + ", selecciona la opción 2 para ver el menu y agregar elementos a tu pedido.");
 	}
@@ -83,123 +84,160 @@ public class Aplicacion {
 		System.out.println("2. VER COMBOS\n");
 		boolean continuar = true;
 		while (continuar) {
-			int menu = Integer.parseInt(input("Ingresa 1 para ver los productos y 2 para ver los combos"));
-			//Agregar productos
-			if (menu == 1) {
-				continuar = false;
-				System.out.println("\n--------------- PRODUCTOS ---------------\n");
-				ArrayList<ProductoMenu> productosMenu = restaurante.getMenuBase();
-				for (int i = 0; i < productosMenu.size(); i++) {
-					ProductoMenu valorP = productosMenu.get(i);
-					System.out.println((i+1) + ". " + valorP.getNombre() + " ----------------- $" + valorP.getPrecio());
-				}
-				boolean continuarP = true;
-				while (continuarP) {
-					int numProducto = Integer.parseInt(input("\nIngresa el numero del producto que deseas agregar"));
-					if (numProducto > productosMenu.size())
-						System.out.println("\nPor favor ingresa una opción válida.\n");
-					else {
-						continuarP = false;
-						ProductoMenu valorP = productosMenu.get(numProducto-1);
-						
-						//Agregar o quitar ingrediente
-						boolean continuar0 = true;
-						while (continuar0) {
-							int modificar = Integer.parseInt(input("\nPara agregar o quitar algún ingrediente ingresa 1. De lo contrario ingresa 0"));
-							//modificar
-							if (modificar == 1){
-								continuar0 = false;
-								boolean continuar1 = true;
-								while (continuar1) {
-									System.out.println("\n--------------- INGREDIENTES ---------------\n");
-									ArrayList<Ingrediente> ingredientes = restaurante.getIngredientes();
-									for (int i = 0; i < ingredientes.size(); i++) {
-										Ingrediente valorI = ingredientes.get(i);
-										System.out.println((i+1) + ". " + valorI.getNombre() + " ----------------- $" + valorI.getCostoAdicional());
-									}
-									int numIngrediente = Integer.parseInt(input("\nIngresa el numero del ingrediente que deseas agregar o quitar"));
-									Ingrediente valorI = ingredientes.get(numIngrediente-1);
-									boolean continuar12 = true;
-									while (continuar12) {
-										int accionIngrediente = Integer.parseInt(input("\nIngresa 1 para agregar el ingrediente o 0 para quitarlo"));
-										if (accionIngrediente == 1) {
-											//agregar ingrediente y producto modificado
-											
-											//modificar precio
-											total += valorI.getCostoAdicional();
-											continuar1 = false;
-											continuar12 = false;
+			try {
+				int menu = Integer.parseInt(input("Ingresa 1 para ver los productos y 2 para ver los combos"));
+				//Agregar productos
+				if (menu == 1) {
+					continuar = false;
+					System.out.println("\n--------------- PRODUCTOS ---------------\n");
+					ArrayList<ProductoMenu> productosMenu = restaurante.getMenuBase();
+					for (int i = 0; i < productosMenu.size(); i++) {
+						ProductoMenu valorP = productosMenu.get(i);
+						System.out.println((i+1) + ". " + valorP.getNombre() + " ----------------- $" + valorP.getPrecio());
+					}
+					boolean continuarP = true;
+					while (continuarP) {
+						try {
+							int numProducto = Integer.parseInt(input("\nIngresa el numero del producto que deseas agregar"));
+							if (numProducto > productosMenu.size())
+								System.out.println("\nPor favor ingresa una opción válida.\n");
+							else {
+								continuarP = false;
+								ProductoMenu valorP = productosMenu.get(numProducto-1);
+								
+								//Agregar o quitar ingrediente
+								boolean continuar0 = true;
+								while (continuar0) {
+									try {
+										int modificar = Integer.parseInt(input("\nPara agregar o quitar algún ingrediente ingresa 1. De lo contrario ingresa 0"));
+										//modificar
+										if (modificar == 1){
+											continuar0 = false;
+											boolean continuar1 = true;
+											while (continuar1) {
+												try {
+													System.out.println("\n--------------- INGREDIENTES ---------------\n");
+													ArrayList<Ingrediente> ingredientes = restaurante.getIngredientes();
+													for (int i = 0; i < ingredientes.size(); i++) {
+														Ingrediente valorI = ingredientes.get(i);
+														System.out.println((i+1) + ". " + valorI.getNombre() + " ----------------- $" + valorI.getCostoAdicional());
+													}
+													int numIngrediente = Integer.parseInt(input("\nIngresa el numero del ingrediente que deseas agregar o quitar"));
+													Ingrediente valorI = ingredientes.get(numIngrediente-1);
+													boolean continuar12 = true;
+													while (continuar12) {
+														try {
+															int accionIngrediente = Integer.parseInt(input("\nIngresa 1 para agregar el ingrediente o 0 para quitarlo"));
+															if (accionIngrediente == 1) {
+																// ! ! ! ! 
+																
+																modificacion += " con " + valorI.getNombre();
+																total += valorI.getCostoAdicional();
+																continuar1 = false;
+																continuar12 = false;
+															}
+															else if (accionIngrediente == 0) {
+																modificacion += " sin " + valorI.getNombre(); 
+																//quitar ingrediente
+																continuar1 = false;
+																continuar12 = false;
+															}
+															else
+																System.out.println("\nPor favor ingresa una opción válida.\n");
+														}
+														catch (NumberFormatException e)
+														{
+															System.out.println("\nPor favor ingresa una opción válida.\n");
+														}
+													}
+													boolean continuar2 = true;
+													while (continuar2) {
+														try {
+															int seguir = Integer.parseInt(input("Para seguir agregando o quitando ingredientes del producto " + valorP.getNombre() + " ingresa 1. De lo contrario ingresa 0"));
+															if (seguir == 1){
+																continuar1 = true;
+																continuar2 = false;
+															}
+															else if (seguir == 0) {
+																//pedido.agregarProducto(valorP);
+																total += valorP.getPrecio();
+																System.out.println("\nEl producto " + valorP.getNombre() + modificacion + " se agregó correctamente a tu pedido.");
+																System.out.println("\nTotal: $" + total);
+																System.out.println("Para seguir agregando elementos selecciona la opción 2.");
+																continuar2 = false;
+															}
+															else
+																System.out.println("\nPor favor ingresa una opción válida.\n");
+														}
+														catch (NumberFormatException e)
+														{
+															System.out.println("\nPor favor ingresa una opción válida.\n");
+														}
+													}
+												}
+												catch (NumberFormatException e)
+												{
+													System.out.println("\nPor favor ingresa una opción válida.\n");
+												}
+											}
 										}
-										else if (accionIngrediente == 0) {
-											//quitar ingrediente
-											continuar1 = false;
-											continuar12 = false;
-										}
-										else
-											System.out.println("\nPor favor ingresa una opción válida.\n");
-									}
-									boolean continuar2 = true;
-									while (continuar2) {
-										int seguir = Integer.parseInt(input("Para seguir agregando o quitando ingredientes del producto " + valorP.getNombre() + " ingresa 1. De lo contrario ingresa 0"));
-										if (seguir == 1){
-											continuar1 = true;
-											continuar2 = false;
-										}
-										else if (seguir == 0) {
-											//pedido.agregarProducto(valorP);
+										//NO modificar
+										else if (modificar == 0) {
+											continuar0 = false;
+											pedido.agregarProducto(valorP);
 											total += valorP.getPrecio();
 											System.out.println("\nEl producto " + valorP.getNombre() + " se agregó correctamente a tu pedido.");
 											System.out.println("\nTotal: $" + total);
 											System.out.println("Para seguir agregando elementos selecciona la opción 2.");
-											continuar2 = false;
 										}
-										else
+										else 
 											System.out.println("\nPor favor ingresa una opción válida.\n");
+									}
+									catch (NumberFormatException e)
+									{
+										System.out.println("\nPor favor ingresa una opción válida.\n");
 									}
 								}
 							}
-							//NO modificar
-							else if (modificar == 0) {
-								continuar0 = false;
-								pedido.agregarProducto(valorP);
-								total += valorP.getPrecio();
-								System.out.println("\nEl producto " + valorP.getNombre() + " se agregó correctamente a tu pedido.");
-								System.out.println("\nTotal: $" + total);
-								System.out.println("Para seguir agregando elementos selecciona la opción 2.");
-							}
-							else 
-								System.out.println("\nPor favor ingresa una opción válida.\n");
+						}
+						catch (NumberFormatException e)
+						{
+							System.out.println("\nPor favor ingresa una opción válida.\n");
 						}
 					}
 				}
-			}
-			//Agregar combo
-			else if (menu == 2){
-				continuar = false;
-				System.out.println("\n--------------- COMBOS ---------------\n");
-				ArrayList<Combo> combos = restaurante.getCombos();
-				for (int i = 0; i < combos.size(); i++) {
-					Combo valorC = combos.get(i);
-					System.out.println((i+1) + ". " + valorC.getNombre() + " ----------------- $" + valorC.getPrecio());
-				}
-				boolean continuarC = true;
-				while (continuarC) {
-					int numCombo = Integer.parseInt(input("\nIngresa el numero del combo que deseas agregar"));
-					if (numCombo > combos.size())
-						System.out.println("\nPor favor ingresa una opción válida.\n");
-					else {
-						continuarC = false;
-						Combo valorC = combos.get(numCombo-1);
-						pedido.agregarProducto(valorC);
-						total += valorC.getPrecio();
-						System.out.println("\nEl combo " + valorC.getNombre() + " se agregó correctamente a tu pedido.");
-						System.out.println("\nTotal: $" + total);
-						System.out.println("Para seguir agregando elementos selecciona la opción 2.");
+				//Agregar combo
+				else if (menu == 2){
+					continuar = false;
+					System.out.println("\n--------------- COMBOS ---------------\n");
+					ArrayList<Combo> combos = restaurante.getCombos();
+					for (int i = 0; i < combos.size(); i++) {
+						Combo valorC = combos.get(i);
+						System.out.println((i+1) + ". " + valorC.getNombre() + " ----------------- $" + valorC.getPrecio());
+					}
+					boolean continuarC = true;
+					while (continuarC) {
+						int numCombo = Integer.parseInt(input("\nIngresa el numero del combo que deseas agregar"));
+						if (numCombo > combos.size())
+							System.out.println("\nPor favor ingresa una opción válida.\n");
+						else {
+							continuarC = false;
+							Combo valorC = combos.get(numCombo-1);
+							pedido.agregarProducto(valorC);
+							total += valorC.getPrecio();
+							System.out.println("\nEl combo " + valorC.getNombre() + " se agregó correctamente a tu pedido.");
+							System.out.println("\nTotal: $" + total);
+							System.out.println("Para seguir agregando elementos selecciona la opción 2.");
+						}
 					}
 				}
+				else
+					System.out.println("\nPor favor ingresa una opción válida.\n");
 			}
-			else
+			catch (NumberFormatException e)
+			{
 				System.out.println("\nPor favor ingresa una opción válida.\n");
+			}
 		}
 	}
 	
